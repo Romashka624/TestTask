bed7f863612b76a1f92f27b66708e9dc1f5e4bb6
Roman <roman.vynarovych@gmail.com>
2020-03-20 13:54:18 +0200

    Init

---
+++ Test 1/Teast 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Teast_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for(int g = 0; g < mas.Length; g++)
+            {
+                if(g < mas.Length - 1)
+                {
+                    Console.Write("{0}, ", mas[g]);
+                }
+                else
+                {
+                    Console.Write("{0}. ", mas[g]);
+                }
+            }
+            Console.WriteLine("\nInsert value: ");
+            int num =  Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < mas.Length; i++)
+            {
+                for( int k = i; k < mas.Length; k++)
+                {
+                    if( mas[i] + mas[k] == num)
+                    {
+                        Console.WriteLine("{0}({1}) + {2}({3}) = {4}", mas[i], i, mas[k], k, num);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Test 2/Test 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Test_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert the value: \n");
+            string st = Console.ReadLine();
+            string k = "";
+            for(int i = st.Length - 1; i >= 0; i--)
+            {
+                k = k + st[i];
+            }
+            if(k == st)
+            {
+                Console.WriteLine("\"{0} is palindrome. {0} == {1}", st, k);
+            }
+            else
+            {
+                Console.WriteLine("\"{0} is not palindrome. {0} != {1}", st, k);
+            }
+        }
+    }
+}
